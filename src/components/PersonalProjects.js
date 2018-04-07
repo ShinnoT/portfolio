73f27778e4b2433f9ab4e2bd59b6ed545c5c592f
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2018-04-07 19:05:19 +0900

    projects skeleton

--- src/components/PersonalProjects.js
+++ src/components/PersonalProjects.js
@@ -3,7 +3,12 @@ import React, { Component } from "react";
 class PersonalProjects extends Component {
   render() {
     return (
-      <div className="PersonalProjects">{/* <h1>PersonalProjects</h1> */}</div>
+      <div className="PersonalProjects">
+        <div className="projects">
+          <h1>Personal Projects</h1>
+          <div className="projects-list">projects to display here</div>
+        </div>
+      </div>
     );
   }
 }
