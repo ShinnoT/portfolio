13e4712e7f44dbb421498de73d2e7eba95cd00c7
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2018-04-09 12:37:57 +0900

    cropped images imported with hover effect

--- src/components/PersonalProjects.js
+++ src/components/PersonalProjects.js
@@ -1,12 +1,48 @@
 import React, { Component } from "react";
 
+const staticChat = require("./../cropped-chat.png");
+const gifChat = require("./../cropped-chat.gif");
+
 class PersonalProjects extends Component {
   render() {
     return (
       <div className="PersonalProjects">
         <div className="projects">
           <h1>Personal Projects</h1>
-          <div className="projects-list">projects to display here</div>
+          <div className="projects-list">
+            <div className="gif">
+              <img src={staticChat} alt="" className="static" />
+              <img src={gifChat} alt="" className="non-static" />
+            </div>
+            <div className="gif">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Bicycle.svg/444px-Bicycle.svg.png"
+                alt=""
+                className="static"
+              />
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Bananas.svg/560px-Bananas.svg.png"
+                alt=""
+                className="non-static"
+              />
+            </div>
+            <div className="gif">
+              <img src="" alt="" className="static" />
+              <img src="" alt="" className="non-static" />
+            </div>
+            <div className="gif">
+              <img src="" alt="" className="static" />
+              <img src="" alt="" className="non-static" />
+            </div>
+            <div className="gif">
+              <img src="" alt="" className="static" />
+              <img src="" alt="" className="non-static" />
+            </div>
+            <div className="gif">
+              <img src="" alt="" className="static" />
+              <img src="" alt="" className="non-static" />
+            </div>
+          </div>
         </div>
       </div>
     );
