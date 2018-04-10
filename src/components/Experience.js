51ef49605c6371efb77b9e9495d9116ea9fe26a7
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2018-04-10 13:24:44 +0900

    experiences skeleton

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,7 +2,22 @@ import React, { Component } from "react";
 
 class Experience extends Component {
   render() {
-    return <div className="Experience" />;
+    return (
+      <div className="Experience">
+        <h1>Experience</h1>
+        <div className="job-content">
+          <div className="job">
+            <h1>Tokyo Hearth</h1>
+          </div>
+          <div className="job">
+            <h1>Ship&Co</h1>
+          </div>
+          <div className="job">
+            <h1>Le Wagon</h1>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
